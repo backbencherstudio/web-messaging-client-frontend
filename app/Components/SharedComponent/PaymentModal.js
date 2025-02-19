93bd61c041a11ec53c7481c90cf9e81dbd31f4de
sohan284 <sr.sohan088@gmail.com>
2025-02-19 16:35:18 +0600

    add responsive

--- app/Components/SharedComponent/PaymentModal.js
+++ app/Components/SharedComponent/PaymentModal.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -17,63 +17,64 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-
 const PaymentModal = ({ open, onClose, setOpenSuccess }) => {
   const handleSuccess = () => {
     setOpenSuccess(true);
     onClose();
   };
   return (
-
-        <Dialog open={open} onOpenChange={onClose} >
-          <DialogContent className="lg:max-w-[1080px] text-lg mx-5 md:mx-4 lg:mx-0 rounded-lg">
-            <DialogHeader>
-              <DialogTitle className="text-2xl">Payment</DialogTitle>
-            </DialogHeader>
-            <div className="space-y-4">
-              <Input placeholder="Your Email" className="text-lg p-8" />
-              <Input placeholder="Amount ($)" className="text-lg p-8" />
-              <div className="flex gap-2">
-                <Button variant="outline" className="text-lg p-8">
-                  Card
-                </Button>
-                <Button variant="outline" className="text-lg p-8">
-                  EPS
-                </Button>
-                <Button variant="outline" className="text-lg p-8">
-                  Giropay
-                </Button>
-              </div>
-              <Input placeholder="Card number" className="text-lg p-8" />
-              <div className="flex gap-2">
-                <Input placeholder="MM / YY" className="text-lg p-8" />
-                <Input placeholder="CVC" className="text-lg p-8" />
-              </div>
-              <div className="flex gap-2">
-                <Select>
-                  <SelectTrigger className="text-lg p-8">
-                    <SelectValue placeholder="Country" />
-                  </SelectTrigger>
-                  <SelectContent className="text-lg">
-                    <SelectItem value="us">United States</SelectItem>
-                    <SelectItem value="ca">Canada</SelectItem>
-                  </SelectContent>
-                </Select>
-                <Input placeholder="Postal code" className="text-lg p-8" />
-              </div>
-              <div className="flex items-center gap-2">
-                <input type="checkbox" id="terms" className="h-5 w-5" />
-                <label htmlFor="terms" className="text-base">
-                  By agreeing to this, you accept the terms. This payment is
-                  non-refundable.
-                </label>
-              </div>
-              <Button onClick={handleSuccess} className="w-[234px] py-6 px-12 text-lg rounded-full">
-                Payment & Submit
-              </Button>
-            </div>
-        </DialogContent>
-      </Dialog>
+    <Dialog open={open} onOpenChange={onClose}>
+      <DialogContent className=" text-lg p-6 px-5  pt-[80px] pb-[70px] rounded-lg  md:max-w-[1080px] max-w-[90%] ">
+        <DialogHeader>
+          <DialogTitle className="text-2xl">Payment</DialogTitle>
+        </DialogHeader>
+        <div className="space-y-4">
+          <Input placeholder="Your Email" className="text-lg p-8" />
+          <Input placeholder="Amount ($)" className="text-lg p-8" />
+          <div className="flex gap-2">
+            <Button variant="outline" className="text-lg p-8">
+              Card
+            </Button>
+            <Button variant="outline" className="text-lg p-8">
+              EPS
+            </Button>
+            <Button variant="outline" className="text-lg p-8">
+              Giropay
+            </Button>
+          </div>
+          <Input placeholder="Card number" className="text-lg p-8" />
+          <div className="flex gap-2">
+            <Input placeholder="MM / YY" className="text-lg p-8" />
+            <Input placeholder="CVC" className="text-lg p-8" />
+          </div>
+          <div className="flex gap-2">
+            <Select>
+              <SelectTrigger className="text-lg p-8">
+                <SelectValue placeholder="Country" />
+              </SelectTrigger>
+              <SelectContent className="text-lg">
+                <SelectItem value="us">United States</SelectItem>
+                <SelectItem value="ca">Canada</SelectItem>
+              </SelectContent>
+            </Select>
+            <Input placeholder="Postal code" className="text-lg p-8" />
+          </div>
+          <div className="flex items-center gap-2">
+            <input type="checkbox" id="terms" className="h-5 w-5" />
+            <label htmlFor="terms" className="text-base">
+              By agreeing to this, you accept the terms. This payment is
+              non-refundable.
+            </label>
+          </div>
+          <Button
+            onClick={handleSuccess}
+            className="w-[234px] py-6 px-12 text-lg rounded-full"
+          >
+            Payment & Submit
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 };
 
--- app/Components/SharedComponent/SuccessModal.js
+++ app/Components/SharedComponent/SuccessModal.js
@@ -18,24 +18,55 @@ import React from "react";
 
 const SuccessModal = ({ open, onClose }) => {
   return (
-    <div >
-      <Dialog open={open} onOpenChange={onClose} className="" >
+    <div>
+      <Dialog open={open} onOpenChange={onClose} className="">
         <DialogContent className=" text-lg p-6  pt-[80px] pb-[70px] rounded-lg  md:max-w-[528px] max-w-[90%] ">
           <div className="flex ">
-            <div className="flex flex-col items-center justify-center md:w-full  ">
+            <div className="flex flex-col items-center justify-center md:w-full max-w-[80%] overflow-hidden">
               <img className="max-w-[64px]" src="/congo.png" alt="" />
-              <h1 className="text-[#070707] dark:text-[#FDFEFF] leading-normal my-[18px] md:text-[32px] text-2xl  font-medium">Congratulations!</h1>
-              <p className="text-[#393C44] dark:text-[#A8AAB4] leading-[160%] text-center text-base  font-normal max-w-[414px]">Your payment has been successfully processed. Your message has been displayed—please check it now.</p>
+              <h1 className="text-[#070707] dark:text-[#FDFEFF] leading-normal my-[18px] md:text-[32px] text-2xl  font-medium">
+                Congratulations!
+              </h1>
+              <p className="text-[#393C44] dark:text-[#A8AAB4] leading-[160%] text-center text-base  font-normal max-w-[414px]">
+                Your payment has been successfully processed. Your message has
+                been displayed—please check it now.
+              </p>
               <div className="my-[48px]">
-                <svg xmlns="http://www.w3.org/2000/svg" width="442" height="2" viewBox="0 0 442 2" fill="none">
-                  <path d="M1 1H441" stroke="#C9CCD8" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="5 5" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="442"
+                  height="2"
+                  viewBox="0 0 442 2"
+                  fill="none"
+                >
+                  <path
+                    d="M1 1H441"
+                    stroke="#C9CCD8"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-dasharray="5 5"
+                  />
                 </svg>
               </div>
-              <p className="text-[#393C44] dark:text-[#C9CCD8] leading-[160%] text-center text-base  font-medium" style={{ maxWidth: "34ch" }}>Register now to access your user dashboard and
-                manage your message post easily.</p>
+              <p
+                className="text-[#393C44] dark:text-[#C9CCD8] leading-[160%] text-center text-base  font-medium"
+                style={{ maxWidth: "34ch" }}
+              >
+                Register now to access your user dashboard and manage your
+                message post easily.
+              </p>
               <div className="flex gap-4 justify-center flex-row items-center mt-6">
-                <button className="bg-[#070707]  dark:bg-[#F3F6FE] dark:text-[#070707] text-white px-[32px] py-4 rounded-[99px] border-[#070707] text-base font-medium">Register Now</button>
-                <button><a href="" className="text-[#4A4C56] font-normal border-[#4A4C56] border-b dark:text-[#D1D7E5] dark:border-[#D1D7E5] ">Maybe Later</a></button>
+                <button className="bg-[#070707]  dark:bg-[#F3F6FE] dark:text-[#070707] text-white px-[32px] py-4 rounded-[99px] border-[#070707] text-base font-medium">
+                  Register Now
+                </button>
+                <button>
+                  <a
+                    href=""
+                    className="text-[#4A4C56] font-normal border-[#4A4C56] border-b dark:text-[#D1D7E5] dark:border-[#D1D7E5] "
+                  >
+                    Maybe Later
+                  </a>
+                </button>
               </div>
             </div>
           </div>
